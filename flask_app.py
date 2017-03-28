c85472f6f91ebeef89ee5449d0f30bd4ef657b82
aliolin <alexander.li@students.olin.edu>
2017-03-27 21:20:54 -0400

    Flask Toolbox :)

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,14 @@
-"""
-Put your Flask app code here.
-"""
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        return render_template('profile.html', firstname = request.form['name'], age = request.form['age'], favninja = 'Patrick Huston')
+
+if __name__ == '__main__':
+    app.run()
